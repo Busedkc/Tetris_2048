87957ef552f9949d350eac5affbf86e05fbeaf04
Buse <94009706+Busedkc@users.noreply.github.com>
2024-04-20 18:06:17 +0300

    Update Tetris_2048.py

--- Tetris_2048.py
+++ Tetris_2048.py
@@ -57,6 +57,21 @@ def start():
       # check for any user interaction via the keyboard
       if stddraw.hasNextKeyTyped():  # check if the user has pressed a key
          key_typed = stddraw.nextKeyTyped()  # the most recently pressed key
+         # Pause
+         if key_typed == 'p':
+            print("Pause")
+            if pause:
+                pause = False
+            else:
+                pause = True
+        # clear the queue of the pressed keys for a smoother interaction
+        stddraw.clearKeysTyped()
+
+        if not pause:
+            # check user interactions via the keyboard
+            if stddraw.hasNextKeyTyped():  # check if the user has pressed a key
+                key_typed = stddraw.nextKeyTyped()  # the most recently pressed key
+                    
          # if the left arrow key has been pressed
          if key_typed == "left":
             # move the active tetromino left by one
@@ -70,13 +85,35 @@ def start():
             # move the active tetromino down by one
             # (soft drop: causes the tetromino to fall down faster)
             current_tetromino.move(key_typed, grid)
+
+         elif key_typed == "space":
+            # Moves down the tetromino all the way down until it cannot go further
+            if not already_dropped:
+                while True:
+                    sc = current_tetromino.move("down", grid)
+                    if not sc:
+                        break
+                dropped = True
+
+        elif key_typed == "up":
+            current_tetromino.rotate_clockwise()
+
+
+        elif key_typed == "z":
+            current_tetromino.rotate_counterclockwise()
+
+            
          # clear the queue of the pressed keys for a smoother interaction
          stddraw.clearKeysTyped()
 
-      # move the active tetromino down by one at each iteration (auto fall)
-      success = current_tetromino.move("down", grid)
+
+        if not pause:
+            # move the active tetromino down by one at each iteration (auto fall)
+            success = current_tetromino.move("down", grid)
+            
       # lock the active tetromino onto the grid when it cannot go down anymore
-      if not success:
+      
+        if not success and not pause:
          # get the tile matrix of the tetromino without empty rows and columns
          # and the position of the bottom left cell in this matrix
          tiles, pos = current_tetromino.get_min_bounded_tile_matrix(True)
@@ -84,27 +121,59 @@ def start():
          game_over = grid.update_grid(tiles, pos)
          # end the main game loop if the game is over
          if game_over:
+             show_gameOver(grid_w + 4, grid_h - 1.5)
             break
          # create the next tetromino to enter the game grid
          # by using the create_tetromino function defined below
-         current_tetromino = create_tetromino()
-         grid.current_tetromino = current_tetromino
+        current_tetromino = newList[1]
+            grid.current_tetromino = current_tetromino
+            newList.pop(0)
+            newList.append(create_tetromino(grid_h, grid_w))
 
-      # display the game grid with the current tetromino
-      grid.display()
+        # display the game grid and the current tetromino
+        grid.clear()
+        # display the game grid and as well the current tetromino
+        grid.display(pause)
 
-   # print a message on the console when the game is over
-   print("Game over")
+    # print a message on the console when the game is over
+    print("Game over")
 
 # A function for creating random shaped tetrominoes to enter the game grid
 def create_tetromino():
    # the type (shape) of the tetromino is determined randomly
-   tetromino_types = ['I', 'O', 'Z']
+   tetromino_type = ['I', 'O', 'Z','J','T','S','L']
    random_index = random.randint(0, len(tetromino_types) - 1)
    random_type = tetromino_types[random_index]
    # create and return the tetromino
    tetromino = Tetromino(random_type)
    return tetromino
+
+
+def show_gameOver(grid_height, grid_width):
+    # colors used for the menu
+    background_color = Color(255, 182, 193)
+
+    # clear the background canvas to background_color
+    stddraw.clear(background_color)
+    # get the directory in which this python code file is placed
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    # path of the image file
+    img_file = current_dir + "/images/gameover.png"
+    # center coordinates to display the image
+    img_center_x, img_center_y = ((grid_width - 1) / 2) - 0.4, grid_height - 6
+    # image is represented using the Picture class
+    image_to_display = Picture(img_file)
+    # center coordinates to display the image
+    img_center_x, img_center_y = ((grid_width - 1) / 2) - 0.4, grid_height - 6
+    image_to_display = Picture(img_file)
+    # display the image
+    stddraw.picture(image_to_display, img_center_x, img_center_y)
+
+    while True:
+        # display the menu and wait for a short time (50 ms)
+        # menüyü görüntüleyin ve kısa bir süre bekleyin (50 ms)
+        stddraw.show(50)
+
 
 # A function for displaying a simple menu before starting the game
 def display_game_menu(grid_height, grid_width):
